dd47a9cc89d8fca7090d8e076152e67178954fae
3564254 <144762182+3564254@users.noreply.github.com>
2023-11-10 18:01:04 +0900

    Add files via upload

---
+++ hw7.c
@@ -0,0 +1,46 @@
+#define _CRT_SECURE_NO_WARNINGS
+# include <stdio.h>
+
+
+
+int main(void) {
+
+	int arr1[6] = { 1,2,3,4,5,6 };
+	int arr2[6] = { 7,8,9,10,11,12 };
+    int* ptr1 = arr1;
+    int* ptr2 = arr2;
+    int temp;
+    printf("arr1: ");
+    for (int i = 0; i < 6; i++) {
+	printf("%d ", arr1[i]);
+    }
+
+    printf("\n");
+
+    printf("arr2: ");
+    for (int i = 0; i < 6; i++) {
+	    printf("%d ", arr2[i]);
+    }
+
+    printf("\n\nafter swap\n");
+
+    for (int i = 0; i < 6; i++) {
+	    temp = *(arr1 + i);
+	    arr1[i] = arr2[i];
+	    arr2[i] = temp;
+    }
+
+    printf("arr1: ");
+    for (int i = 0; i < 6; i++) {
+	    printf("%d ", arr1[i]);
+    }
+
+    printf("\n");
+
+    printf("arr2: ");
+    for (int i = 0; i < 6; i++) {
+	    printf("%d ", arr2[i]);
+    }
+
+     return 0;
+}
